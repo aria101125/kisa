5d5d5b9411c2359e8f7e9456f95e0e0d9d8d21dd
itoshin-tech <itomakoto3141592@gmail.com>
2021-01-24 15:51:17 +0900

    壁0, 道1以上にここから変更

--- maze.py
+++ maze.py
@@ -20,120 +20,129 @@ import sys
 import pdb
 
 
-
 class Maze():
     """
     迷路生成クラス
     """
     def __init__(self, size_w=10, size_h=5):
         """
-        変数のセット
+        変数をセット
         """
+
+        # 基点の縦横の数
         self.size_w = size_w
         self.size_h = size_h
-        self.render = Render(max_col=size_w)
+
+        # 道生成クラスのインスタンス生成
+        # 引数 max_col は画像出力時のパレット数
+        self.roadcreater =RoadCreater(max_col=size_w)
     
     def generate_maze(self, is_show=True, unit=10, delay=100):
         """
+        道生成
         Returns
         -------
         field: 2d numpuy.array
             生成した迷路
         """
-        self.unit = unit
-        self.delay = delay
 
         # 実際のサイズ
-        self.n_w = 2 * self.size_w + 1
-        self.n_h = 2 * self.size_h + 1
+        n_w = 2 * self.size_w + 1
+        n_h = 2 * self.size_h + 1
 
         # 全てを壁で満たす
-        # 0: 壁, 1: 基点, 2以上:通路
-        self.field = np.zeros((self.n_h, self.n_w), dtype=np.uint8)
+        # 0: 壁, 1: 基点当、表示用
+        # 2以上:通路
+        field = np.zeros((n_h, n_w), dtype=np.uint8)
 
-        # 基点となるセルの座標をランダムな順番でリストに生成
-        self.base_xys = self._get_base()
+        road_id = 2 # 0, 1は壁としている、道のid は2から
 
-        # 起点を1にする
-        for xy in self.base_xys:
-            self.field[xy[1], xy[0]] = 1
-
-        # 全ての起点をランダムな順番で巡回し、
-        # 道がなかったら道を作る
-        result = 0
-        # for j, xy in enumerate(self.base_xys):
-        road_id = 2 # 
-        while True:
-            idxs = np.where(self.field == 1)
-            num = len(idxs[0])
-            if num is 0:
-                break
-            idx = np.random.randint(0, num)
-            # 0: 壁、2以上:通路
-            x = idxs[1][idx]
-            y = idxs[0][idx]
-            if self.field[y, x] > 1:
-                # すでに通路になっていたら次のポイントへ
-                continue
-            
-            # x, y を開始点として、自分以外の壁にぶつかるまで
-            # 道を伸ばす
-            ret = self._create_road(x, y, id=road_id, is_show=is_show)
-            road_id += 1
-            result += (ret == False)
+        # 道の生成
+        x, y = (1, 1)
+        field = self.roadcreater.create_road(
+            field, x, y, id=road_id, is_show=is_show,
+            delay=delay, unit=unit,
+            )
         
-        field = self.get_maze()
+        # 道のid を全て1に変換
+        field_out = field.copy()
+        field_out[field > 0] = 1
+
+        return field_out
+
+
+class RoadCreater:
+    def __init__(self, max_col=10):
+        self.render = Render(max_col=max_col)
+
+    def create_road(self, field, x, y, id=2, is_show=True, delay=100, unit=10):
+        # 開始地点を (x, y) とする
+        # (A)
+        # distance=2 分離れた4方向のセルの状態を調べる
+        # 道を作れる方向（0 or 1）があったら、その中からランダムに選び道を作り、(x, y)を更新。
+        # 道が作れなくなるまで、(A)からを繰り返す。
+        #         
+        # (B)
+        # distance 分もどり、その(x2, y2)からこの関数を開始する。id = id + 1とする
+        # 開始地点に戻るまで(B)を繰り返す。
+
+        self.field = field.copy()
+        self.delay = delay
+        self.unit = unit
         
-        return field
-
-    def get_maze(self):
-        field = self.field.copy()
-        field[field > 0] = 1
-        return field
-
-    def _create_road(self, x, y, id=2, is_show=True):  # (A)
-        # 行き止まりになるまで道を伸ばす
-        # ペン先を1つ戻した位置から、 ret = _create_road() で再帰
-        # 道の根元まで全てもどったら終了
-        # 
-        field = self.field.copy()
-        if field[y, x] < 2:
-            field[y, x] = id
+        # 開始地点が0 or 1 なら id とする
+        if self.field[y, x] < 2:
+            self.field[y, x] = id
+        
+        # xy の履歴 (B)で使用
         xy_history = [(x, y)]
+
+        # (A)
         while True:
-            # x, y から道を伸ばす
-            res, field, x, y, pre_field = self.find_load_from(field, x, y, id)
-            self.field = field
+            # x, y の周囲を調べて、道が伸ばせるならば伸ばす
+            res, x, y, fields = self.extend_road(self.field, x, y, id, distance=2)
+            self.field = fields[-1]
 
             if res == 'stretched':
-                # 道を伸ばした場合は、繰り返す
+                # 道を伸ばした場合は、xy を記録して繰り返す
+                xy_history.append((x, y))
+
+                # 描画
                 if is_show is True:
                     if self.delay > 50:
-                        self.render.draw(pre_field, delay=self.delay, unit=self.unit)
-                    self.render.draw(field, delay=self.delay, unit=self.unit)
-                xy_history.append((x, y))
+                        for ff in fields:
+                            self.render.draw(ff, delay=self.delay, unit=self.unit)
+                    else:
+                        self.render.draw(fields[-1], delay=self.delay, unit=self.unit)
                 continue
 
             # 行き止まりに来たら
             # ループから抜けて行き止まり対策
             if res == 'deadend':
                 break
-
+        
+        # (B)
         # 履歴を一つずつもどった地点から、行き止まりまで道を伸ばす
         xy_history.pop(-1)
         xy_history.reverse()
         for xy in xy_history:
             x = xy[0]
             y = xy[1]
-            self._create_road(x, y, id=id + 1, is_show=is_show)
+            self.create_road(
+                self.field, x, y, id=id + 1, is_show=is_show,
+                delay=self.delay, unit=self.unit,
+                )
+        
+        return self.field
 
-    def find_load_from(self, field, x, y, id):
+    def extend_road(self, field, x, y, id, distance=2):
         dd = [
             (1, 0),
             (-1, 0),
             (0, 1),
             (0, -1),
         ]
+        n_h, n_w = field.shape[:2]
         next_field = field.copy()
         pre_field = field.copy()
         dd_res = [''] * 4
@@ -143,11 +152,11 @@ class Maze():
             x0 = x + dd[id_dir][0]
             y0 = y + dd[id_dir][1]
 
-            if x1 < 0 or self.n_w <= x1:
+            if x1 < 0 or n_w <= x1:
                 dd_res[id_dir] = 'out'
                 continue
 
-            if y1 < 0 or self.n_h <= y1:
+            if y1 < 0 or n_h <= y1:
                 # はみ出したら次の方向を試す
                 dd_res[id_dir] = 'out'
                 continue
@@ -175,120 +184,17 @@ class Maze():
             next_y = y1
 
             pre_field[y0, x0] = id  # アニメーション用の途中状態
+            fields = [pre_field, next_field]
 
-            return res, next_field, next_x, next_y, pre_field
-
-        # 行き止まり
-        res = 'deadend'
-        next_x = None
-        next_y = None
-        return res, next_field, next_x, next_y, pre_field
-
-    def _get_base(self):
-        xs = np.arange(1, self.n_w, 2)
-        ys = np.arange(1, self.n_h, 2)
-        xxs, yys = np.meshgrid(xs, ys)
-        xys = np.vstack([xxs.reshape(-1), yys.reshape(-1)])
-        xys = xys.T
-        xys = xys.tolist()
-        xys = random.sample(xys, k=len(xys))
-        return xys
-
-
-class RoadCreater():
-    def _create_road(self, x, y, id=2, is_show=True):  # (A)
-        # 行き止まりになるまで道を伸ばす
-        # ペン先を1つ戻した位置から、 ret = _create_road() で再帰
-        # 道の根元まで全てもどったら終了
-        # 
-        field = self.field.copy()
-        if field[y, x] < 2:
-            field[y, x] = id
-        xy_history = [(x, y)]
-        while True:
-            # x, y から道を伸ばす
-            res, field, x, y, pre_field = self.find_load_from(field, x, y, id)
-            self.field = field
-
-            if res == 'stretched':
-                # 道を伸ばした場合は、繰り返す
-                if is_show is True:
-                    if self.delay > 50:
-                        self.render.draw(pre_field, delay=self.delay, unit=self.unit)
-                    self.render.draw(field, delay=self.delay, unit=self.unit)
-                xy_history.append((x, y))
-                continue
-
-            # 行き止まりに来たら
-            # ループから抜けて行き止まり対策
-            if res == 'deadend':
-                break
-
-        # 履歴を一つずつもどった地点から、行き止まりまで道を伸ばす
-        xy_history.pop(-1)
-        xy_history.reverse()
-        for xy in xy_history:
-            x = xy[0]
-            y = xy[1]
-            self._create_road(x, y, id=id + 1, is_show=is_show)
-
-    def find_load_from(self, field, x, y, id):
-        dd = [
-            (1, 0),
-            (-1, 0),
-            (0, 1),
-            (0, -1),
-        ]
-        next_field = field.copy()
-        pre_field = field.copy()
-        dd_res = [''] * 4
-        for id_dir in range(4):
-            x1 = x + dd[id_dir][0] * 2
-            y1 = y + dd[id_dir][1] * 2
-            x0 = x + dd[id_dir][0]
-            y0 = y + dd[id_dir][1]
-
-            if x1 < 0 or self.n_w <= x1:
-                dd_res[id_dir] = 'out'
-                continue
-
-            if y1 < 0 or self.n_h <= y1:
-                # はみ出したら次の方向を試す
-                dd_res[id_dir] = 'out'
-                continue
-
-            if field[y1, x1] in (0, 1):
-                # 壁
-                dd_res[id_dir] = 'wall'
-                continue
-
-            # 道
-            dd_res[id_dir] = 'same_id'
-
-        # 壁が周りにあったらそこに道を伸ばしてもどる
-        ids_dir = [i for i, x in enumerate(dd_res) if x == 'wall']
-        if len(ids_dir) > 0:
-            res = 'stretched'
-            id_dir = random.sample(ids_dir, 1)[0]
-            x1 = x + dd[id_dir][0] * 2
-            y1 = y + dd[id_dir][1] * 2
-            x0 = x + dd[id_dir][0]
-            y0 = y + dd[id_dir][1]
-            next_field[y1, x1] = id
-            next_field[y0, x0] = id
-            next_x = x1
-            next_y = y1
-
-            pre_field[y0, x0] = id  # アニメーション用の途中状態
-
-            return res, next_field, next_x, next_y, pre_field
+            return res, next_x, next_y, fields
 
         # 行き止まり
         res = 'deadend'
         next_x = None
         next_y = None
-        return res, next_field, next_x, next_y, pre_field
-            
+        fields = [pre_field, next_field]
+        return res, next_x, next_y, fields
+
 
 class Render():
     """
@@ -378,8 +284,8 @@ class MazeSolver:
         
 if __name__ == '__main__':
 
-    # ptype = 'anime'
-    ptype = 'solve'
+    ptype = 'anime'
+    # ptype = 'solve'
 
     if ptype == 'anime':
         ws = [10, 15, 20, 40]
@@ -392,10 +298,10 @@ if __name__ == '__main__':
                 field = maze.generate_maze(
                     is_show=True, unit=u, delay=d,
                     )
-                maze.render.draw(
+                maze.roadcreater.render.draw(
                     field, unit=u, delay=2000, unicol=(200, 200, 200),
                     )
-        maze.render.draw(
+        maze.roadcreater.render.draw(
             field, unit=u, delay=0, unicol=(200, 200, 200),
             )
         
@@ -405,13 +311,11 @@ if __name__ == '__main__':
         maze = Maze(size_w=w, size_h=h)
         field = maze.generate_maze(is_show=True, unit=u, delay=d)
         print(field)
-        maze.render.draw(field, unit=u, delay=0, unicol=(200, 200, 200))
+        maze.roadcreater.render.draw(field, unit=u, delay=0, unicol=(200, 200, 200))
 
-        solver = MazeSolver(field, start=(1, 1), goal=(1, 2))
-        map = solver.solve_maze(is_show=True, unit=u, delay=100)
-        print(1)
-        solver.render.draw(map, unit=u, delay=0)
-        print(2)
+        # solver = MazeSolver(field, start=(1, 1), goal=(1, 2))
+        # map = solver.solve_maze(is_show=True, unit=u, delay=60)
+        # solver.render.draw(map, unit=u, delay=0)
 
 
 
